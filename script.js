2222b884b90a63c57a24eba310f35fa4911d267c
leirsam <leirsam@gmail.com>
2025-12-07 21:35:16 +0100

    first commit: created the inhtml, css and js files, added the basic buttons in html and stared the basic css styling by creating the .container and created the js function related to operators

---
+++ script.js
@@ -0,0 +1,45 @@
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((total, current) => total*current, 1);
+};
+
+const power = function(a,b) {
+	return a**b;
+};
+
+const factorial = function(n) {
+	if (n<=1) return 1;
+  return n*factorial(n-1);
+};
+
+let num1 = '';
+let num2 = '';
+let operator = null;
+
+const operate = function(num1 ,num2 , operator) {
+    switch (operator) {
+        case "+": return add(num1, num2);
+        break;
+        case "-": return subtract(num1, num2);
+        break;
+        case "*": return multiply(num1, num2);
+        break;
+        case "**": return power(num1, num2);
+        break;
+    } 
+
+}
+
+
+
